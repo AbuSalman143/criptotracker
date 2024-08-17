d79f8f15188237a8a9c66fbf30fe9f16713d0bdc
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-08-17 17:36:26 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Input from './component/Input'
+
+// import { useState } from 'react';
+
+function App() {
+  
+  
+
+  return (
+    
+    
+    <Input />
+    
+   
+  )
+}
+
+
+export default App;
